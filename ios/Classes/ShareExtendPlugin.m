23e996bb4f4ce1271d4af08ba3d626ee2c65b193
zhangxueAragon <zhangxue@aragoncs.com>
2024-01-24 22:09:48 +0800

    add image when share url

--- ios/Classes/ShareExtendPlugin.m
+++ ios/Classes/ShareExtendPlugin.m
@@ -78,6 +78,21 @@
     }];
 }
 
++ (UIImage*)imageWithImageSimple:(UIImage*)image scaledToSize:(CGSize)newSize
+{
+    // Create a graphics image context
+    UIGraphicsBeginImageContext(newSize);
+    // Tell the old image to draw in this new context, with the desired
+    // new size
+    [image drawInRect:CGRectMake(0,0,newSize.width,newSize.height)];
+    // Get the new image from the context
+    UIImage* newImage = UIGraphicsGetImageFromCurrentImageContext();
+    // End the context
+    UIGraphicsEndImageContext();
+    // Return the new image.
+    return newImage;
+}
+
 + (void)share:(NSArray *)sharedItems atSource:(CGRect)origin withSubject:(NSString *) subject {
     UIActivityViewController *activityViewController = [[UIActivityViewController alloc] initWithActivityItems:sharedItems applicationActivities:nil];
     
